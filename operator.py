5d0ebebea48d64e8937cf8bd582012445a93e595
arshima-pathan <aashimak031@gmail.com>
2026-01-27 09:01:35 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,49 @@
+"""#Arithmetic operator
+a=5
+b=3
+print(a+b)
+print(a-b)
+print(a/b)
+
+#assignment operator
+x=20
+print(x)
+
+#unary minus
+a=4
+print(-a)
+
+#relational operator
+p=15
+q=5
+print(p>q)
+print(p<q)
+print(p==q)
+
+#logical operator
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#boolean values
+is_pass=True
+print(is_pass)
+
+#bitwise operator
+a=5
+b=6
+print(a&b)
+print(a/b)
+
+#membership operator
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers)"""
+
+#Identity operator
+x=5
+y=10
+print(x is y)
+print(x is not y)
